64176d78504cfd168e68bc8e2e319bfa67cbb648
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 15:39:18 -0500

    Updated initial test

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -2,12 +2,19 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import App from '../App'
 
-describe('App Component', () => {
-	test('renders Task Manager heading', () => {
-		render(<App />)
-		const headingElement = screen.getByText(/Task Manager/i)
+describe('Task Manager heading', () => {
+	render(<App />)
+	const headingElement = screen.getByText(/Task Manager/i)
+	test('exists', () => {
 		expect(headingElement).toBeInTheDocument()
-		expect(headingElement).toHaveClass('text-2xl', 'font-bold')
+	})
+	test('is within an H1 tag', () => {
 		expect(headingElement.tagName).toBe('H1')
+	})
+	test('is size 2xl', () => {
+		expect(headingElement).toHaveClass('text-2xl')
+	})
+	test('is bold', () => {
+		expect(headingElement).toHaveClass('font-bold')
 	})
 })
