c96676d286396d6f259764467c83e6468e9d1a22
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 14:05:23 -0500

    Connected to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { connectDB } from './db/connect.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -8,5 +9,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
+	connectDB()
 	console.log(`🚀 Server is running on port ${PORT}`)
 })
