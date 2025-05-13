8118f6e30902fb0af0525fc679f2391f498b49fe
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 15:47:24 -0500

    Moved render into a beforeEach hook #6

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -3,18 +3,23 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 describe('Task Manager heading', () => {
-	render(<App />)
-	const headingElement = screen.getByText(/Task Manager/i)
+	beforeEach(() => {
+		render(<App />)
+	})
 	test('exists', () => {
+		const headingElement = screen.getByText(/Task Manager/i)
 		expect(headingElement).toBeInTheDocument()
 	})
 	test('is within an H1 tag', () => {
+		const headingElement = screen.getByText(/Task Manager/i)
 		expect(headingElement.tagName).toBe('H1')
 	})
 	test('is size 2xl', () => {
+		const headingElement = screen.getByText(/Task Manager/i)
 		expect(headingElement).toHaveClass('text-2xl')
 	})
 	test('is bold', () => {
+		const headingElement = screen.getByText(/Task Manager/i)
 		expect(headingElement).toHaveClass('font-bold')
 	})
 })
