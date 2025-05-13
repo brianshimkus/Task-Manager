8b8368e9ce0b78ae8f320ef48858854221129eaf
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 14:29:53 -0500

    Task model

---
+++ backend/models/task.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const taskSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: ['to-do', 'ready', 'in-progress', 'blocked', 'done'],
+			default: 'to-do',
+		},
+		dueDate: {
+			type: Date,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+)
+
+const Task = mongoose.model('Task', taskSchema)
+
+export default Task
